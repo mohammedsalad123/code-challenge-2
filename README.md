0cc13a67f261b0f4706f818ed2f0fc57820262a2
mohammed <mohammed.salad@student.moringaschool.com>
2023-10-16 06:54:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#code challenge#
+ #steps in Html #
+ 1 created two divs in the body which will display
+    1 animal list
+    2 animal details 
+
+
+
+ #steps in script.js
+ 1 feched the data from JSON using fetch method then reterived that using the then method.
+ 2 declared element from html using dom selector method
+ 3 function to display animal list and has an eventlistener
+ 4function which displays animalname,image&votecount
+ 5 added abutton and in it there is addeventlisterner.
+ 6 reset button that resets the votes
---
+++ script.js
@@ -0,0 +1,58 @@
+const baseURL = 'http://localhost:3000/characters'
+fetch(baseURL)
+.then(res => res.json())
+.then(data=>{displayAnimalsLists(data)})
+.catch(error=>console.log(error))
+
+
+//declared element from html using dom selector method
+const animalLists = document.querySelector("#animal_list")
+const animalDetails= document.querySelector("#animal_details")
+const resetButton = document.getElementById("reset-button")
+
+
+//function to display animal list and has an eventlistener 
+function displayAnimalsLists(data){
+  animalLists.innerHTML='';
+  data.forEach(animal => {
+    const animalName = document.createElement('div');
+    animalName.textContent=animal.name
+    animalName.classList.add('animal-name');
+    animalName.addEventListener('click', () => {
+        displayAnimalDetails(animal)
+  });
+  animalLists.appendChild(animalName)
+    })
+}
+//function which displays animalname,image&votecount.
+function displayAnimalDetails(animal){
+    animalDetails.innerHTML='';
+    const animalName = document.createElement('h2');
+    animalName.textContent = animal.name
+
+    const animalImage = document.createElement('img');
+    animalImage.src = animal.image;
+
+    const voteCount = document.createElement('p');
+    voteCount.textContent = `Votes: ${animal.votes}`;
+
+
+    const voteButton = document.createElement('button')//added abutton and in it there is addeventlisterner.
+    voteButton.textContent = 'Vote';
+    voteButton.addEventListener('click', () => {
+        animal.votes++;
+        voteCount.textContent = `Votes: ${animal.votes}`;
+    });
+    animalDetails.appendChild(animalName);
+    animalDetails.appendChild(animalImage);
+    animalDetails.appendChild(voteCount);
+    animalDetails.appendChild(voteButton);
+}
+//reset button that resets the votes
+resetButton.addEventListener("click",()=>{
+    animalData.characters.forEach(animal => {
+        animal.votes = 0;
+    });
+    displayAnimalList();
+    animalDetails.innerHTML = '';
+})
